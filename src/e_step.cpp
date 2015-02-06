2f46b2b7d17ed2ff2a08e4f148759f03731911f3
chandlerzuo <chandler.c.zuo@gmail.com>
2015-02-06 11:24:31 +0000

    updating to dynamic scoping

--- src/e_step.cpp
+++ src/e_step.cpp
@@ -183,8 +183,8 @@ SEXP e_step( SEXP _W, SEXP _P, SEXP _zeta, SEXP _probz, SEXP _PDF ) {
 				double tmp1;
 				tmp1 = b_mean( i ) * exp( PDF( k, i + s * I ) - FP( k, i ) ) * P( i, s );
 				Theta_b( i, s ) += tmp1;
-				if(i == 7) {
-					printf("i=7, k=%d, s=%d, Theta_b(i, s) += %3.3f\n b_mean=%3.3f, PDF=%3,3f, FP=%3.3f, P=%3.3f\n\n", k, s, tmp1, b_mean(i), PDF(k, i + s * I), FP(k, i), P(i, s));
+				if(i == 64) {
+					printf("i=7, k=%d, s=%d, Theta_b(i, s) += %3.3f\n b_mean=%3.3f, PDF=%3.3f, FP=%3.3f, P=%3.3f\n\n", k, s, tmp1, b_mean(i), PDF(k, i + s * I), FP(k, i), P(i, s));
 				} 
 				Theta_mean( k, i + I * s ) = tmp1;
 				for( j = 0; j < J; j ++ ){
@@ -245,7 +245,7 @@ SEXP e_step( SEXP _W, SEXP _P, SEXP _zeta, SEXP _probz, SEXP _PDF ) {
 		double total = 0;
 		for( s = 0; s < S; s ++ )
 			total += Theta_b( i, s );
-		if(i == 7)
+		if(i == 64)
 			printf("total = %3.3f\t", total);
 		for( s = 0; s < S; s ++ ){
 			printf("Theta_b(7, %d) = %3.3f\t", s, Theta_b(i, s)); 
@@ -257,7 +257,8 @@ SEXP e_step( SEXP _W, SEXP _P, SEXP _zeta, SEXP _probz, SEXP _PDF ) {
 				Theta_b( i, s ) = 1 / S;
 			else
 				Theta_b( i, s ) /= total;
-			printf("--> %3.3f\n", Theta_b(i, s)); 
+			if(i == 64)
+				printf("--> %3.3f\n", Theta_b(i, s)); 
 		}
 	}
 
